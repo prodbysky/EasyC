28c091187e9b09428b013d127a73ffbce520b08f
prodbysky <kyrbynyt@gmail.com>
2024-10-08 15:25:20 +0300

    work work work work work work

--- ez_cstr.h
+++ ez_cstr.h
@@ -1,6 +1,8 @@
 #ifndef EZC_H
 #define EZC_H
 
+#include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 
 #ifndef EZC_ASSERT
@@ -23,42 +25,69 @@
 #endif
 
 #define EZC_STR_FMT "%.*s"
-#define EZC_STR_ARG(str) (str).len, (str).str
+#define EZC_STR_ARG(a) (a).len, (a).str
 
-#define EZC_STR_MALLOC_ERR 1
 
 #include <stddef.h>
 
-// Owned C String
+typedef enum {
+    SCE_MALLOC,
+} EZC_StringCreateError;
+
+// Owned C String (String in Rust)
 typedef struct {
     char* str;
     char* head;
     size_t len;
     size_t cap;
-    uint8_t create_error;
-} EzCStr;
+    EZC_StringCreateError create_error;
+} String;
 
-EzCStr EzCStr_make_string_default();
-void EzCstr_pushc(EzCStr* str, char c);
+// Non owning string ref (&str in Rust)
+typedef struct {
+    const char* str;
+    size_t len;
+} StringView;
+
+String EZC_String_make_default();
+String EZC_String_make_with_cap(size_t cap);
+String EZC_String_clone(String* str);
+void EZC_String_push_c(String* str, char c);
+char EZC_String_pop(String* str);
+void EZC_String_c_str(String *str, char* s);
+StringView EZC_StringView_from_String(String str);
 
 #ifdef EZC_IMPL
-EzCStr EzCStr_make_string_default() {
-    char* str = (char*)EZC_MALLOC(sizeof(char) * EZC_DEFAULT_INIT_CAP);
+
+String EZC_String_make_with_cap(size_t cap) {
+    char* str = (char*)EZC_MALLOC(sizeof(char) * cap);
 
     if (str == NULL) {
-        return (EzCStr){
-            .create_error = EZC_STR_MALLOC_ERR
+        return (String){
+            .create_error = SCE_MALLOC
         };
     }
-    return (EzCStr){
+    return (String){
         .str = str,
         .head = str,
         .len = 0,
-        .cap = EZC_DEFAULT_INIT_CAP
+        .cap = cap
     };
+
 }
 
-void EzCstr_pushc(EzCStr* str, char c) {
+String EZC_String_make_default() {
+    return EZC_String_make_with_cap(EZC_DEFAULT_INIT_CAP);
+}
+
+String EZC_String_clone(String* str) {
+    String result = EZC_String_make_with_cap(str->cap);
+    memcpy(result.str, str->str, str->cap);
+    result.len = str->len;
+    return result;
+}
+
+void EZC_String_push_c(String* str, char c) {
     if (str->len + 1 == str->cap) {
         str->str = (char*)realloc(str->str, str->cap * 1.5);
         str->head = str->str + str->len;
@@ -68,6 +97,27 @@ void EzCstr_pushc(EzCStr* str, char c) {
     *str->head = c;
     str->head++;
     str->len++;
+}
+
+char EZC_String_pop(String* str) {
+    char c = *str->head;
+    str->head--;
+    str->len--;
+    return c;
+}
+
+void EZC_String_c_str(String *str, char* s) {
+    while (*s != 0) {
+        EZC_String_push_c(str, *s);
+        s++;
+    }
+}
+
+StringView EZC_StringView_from_String(String str) {
+    return (StringView){
+        .str = str.str,
+        .len = str.len
+    };        
 }
 
 #endif
--- test.c
+++ test.c
@@ -2,15 +2,13 @@
 #define EZC_IMPL
 #include "ez_cstr.h"
 
-int main()
-{
-    EzCStr str = EzCStr_make_string_default();
+int main() {
+    String str = EZC_String_make_default();
 
-    for (int i = 0; i < 16; i++) EzCstr_pushc(&str,'a');
-
-
-    printf(EZC_STR_FMT, EZC_STR_ARG(str));
+    EZC_String_c_str(&str, "Hi!");
+    String clone = EZC_String_clone(&str);
 
 
+    printf(EZC_STR_FMT"\n", EZC_STR_ARG(clone));
     return 0;
 }
